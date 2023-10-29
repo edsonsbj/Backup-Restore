6305c4b458d1bff4627ee570fb343dd9ea5ff40f
edsonsbj <edson_silvasp@hotmail.com>
2023-10-29 11:14:07 -0300

    Optimization Setup.sh

--- setup.sh
+++ setup.sh
@@ -97,7 +97,27 @@ disk_backup() {
   [ -z "$BACKUPDIR" ] ||  BackupDir=$BACKUPDIR
 
   clear
-  }
+
+  mkdir -p $HOME/Scripts
+  cd $HOME/Scripts
+
+    tee -a ./"${BackupRestoreConf}" <<EOF
+# Configuration for Backup-Restore scripts
+
+# TODO: The uuid of the backup drive
+uuid='$uuid'
+
+# TODO: The Backup Drive Mount Point
+BackupDir='$BackupDir'
+
+# Log File
+LogFile="$LogFile"
+
+EOF
+
+clear
+
+}
 
 # Function to backup
 backup() {
@@ -110,35 +130,13 @@ backup() {
 
   clear
 
-  mkdir -p $HOME/Scripts
-  cd $HOME/Scripts
-
-  tee -a ./"${BackupRestoreConf}" <<EOF
-# Configuration for Backup-Restore scripts
-
-# TODO: The uuid of the backup drive
-uuid='$uuid'
-
+    tee -a ./"${BackupRestoreConf}" <<EOF
 # TODO: Directory to backup 
 SourceDir='$SourceDir'
-
-# TODO: The Backup Drive Mount Point
-BackupDir='$BackupDir'
-
-# Log File
-LogFile="$LogFile"
 
 EOF
 
   clear
-
-  wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/Backup.sh
-  wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/Restore.sh
-
-  chmod 700 *.sh
-
-  clear
-
 }
 
 # Function to backup Nextcloud settings
@@ -167,18 +165,7 @@ nextcloud() {
     
   clear
 
-  mkdir -p $HOME/Scripts
-  cd $HOME/Scripts
-
   tee -a ./"${BackupRestoreConf}" <<EOF
-# Configuration for Backup-Restore scripts
-
-# TODO: The uuid of the backup drive
-uuid='$uuid'
-
-# TODO: The Backup Drive Mount Point
-BackupDir='$BackupDir'
-
 # TODO: The service name of the web server. Used to start/stop web server (e.g. 'systemctl start <webserverServiceName>')
 webserverServiceName='$webserverServiceName'
 
@@ -203,19 +190,10 @@ DBUser='$DBUser'
 # TODO: The password of the Nextcloud database user
 DBPassword='$DBPassword'
 
-# Log File
-LogFile="$LogFile"
-
 EOF
 
   clear
 
-  wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Nextcloud/Backup.sh
-  wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Nextcloud/Restore.sh
-
-  chmod 700 *.sh
-
-  clear
 }
 
 # Função para configurar o cron
@@ -245,7 +223,7 @@ clear
 }
 
 # Function to configure the media server
-configure_mediaserver() {
+mediaserver() {
   echo "Choose the media server for backup:"
   select media_server in "Emby" "Jellyfin" "Plex"; do
     case $media_server in
@@ -302,18 +280,7 @@ configure_mediaserver() {
 
   clear 
 
-  mkdir -p $HOME/Scripts
-  cd $HOME/Scripts
-
   tee -a ./"${BackupRestoreConf}" <<EOF
-# Configuration for Backup-Restore scripts
-
-# TODO: The uuid of the backup drive
-uuid='$uuid'
-
-# TODO: The Backup Drive Mount Point
-BackupDir='$BackupDir'
-
 # TODO: The service name of the media server. Used to start/stop web server (e.g. 'systemctl start <mediaserverServiceName>')
 MediaserverService='$MediaserverService'
 
@@ -323,158 +290,7 @@ MediaserverUser='$MediaserverUser'
 # TODO: The directory where the Media Server settings are stored (this directory is stored within /var/lib)
 MediaserverConf='$MediaserverConf'
 
-# Log File
-LogFile="$LogFile"
-
 EOF
-
-  clear
-
-  wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Media%20Server/Backup.sh
-  wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Media%20Server/Restore.sh
-
-  chmod 700 *.sh
-
-  clear
-
-}
-
-# Function to configure the media server with NextCloud
-configure_nextcloud_mediaserver() {
-  echo "Enter the path to the Nextcloud file directory."
-  echo "Usually: ${NextcloudConfig}"
-  echo ""
-  read -p "Enter a directory or press ENTER if the file directory is ${NextcloudConfig}: " NEXTCLOUDCONF
-
-  [ -z "$NEXTCLOUDCONF" ] ||  NextcloudConfig=$NEXTCLOUDCONF
-  clear
-
-  echo "Enter the webserver service name."
-  echo "Usually: nginx or apache2"
-  echo ""
-  read -p "Enter an new webserver service name or press ENTER if the webserver service name is ${webserverServiceName}: " WEBSERVERSERVICENAME
-
-  [ -z "$WEBSERVERSERVICENAME" ] ||  webserverServiceName=$WEBSERVERSERVICENAME
-  clear
-
-  NextcloudDataDir=$(sudo -u www-data php $NextcloudConfig/occ config:system:get datadirectory)
-  DatabaseSystem=$(sudo -u www-data php $NextcloudConfig/occ config:system:get dbtype)
-  NextcloudDatabase=$(sudo -u www-data php $NextcloudConfig/occ config:system:get dbname)
-  DBUser=$(sudo -u www-data php $NextcloudConfig/occ config:system:get dbuser)
-  DBPassword=$(sudo -u www-data php $NextcloudConfig/occ config:system:get dbpassword)
-
-  echo "Choose the Backup Media Server: "
-  select media_server in "Emby" "Jellyfin" "Plex"; do
-    case $media_server in
-      "Emby")
-        MediaserverService="emby"
-        MediaserverUser="emby"
-
-        echo "Enter the path to the Emby file directory."
-        echo "Usually: ${Emby_Conf}"
-        echo ""
-        read -p "Enter a directory or press ENTER if the file directory is ${Emby_Conf}: " EMBY_CONF
-
-        [ -z "$EMBY_CONF" ] ||  Emby_Conf=$EMBY_CONF
-        clear
-
-        MediaserverConf="$Emby_Conf"
-        break
-        ;;
-      "Jellyfin")
-        MediaserverService="jellyfin"
-        MediaserverUser="jellyfin"
-
-        echo "Enter the path to the Jellyfin file directory."
-        echo "Usually: ${Jellyfin_Conf}"
-        echo ""
-        read -p "Enter a directory or press ENTER if the file directory is ${Jellyfin_Conf}: " JELLYFIN_CONF
-
-        [ -z "$JELLYFIN_CONF" ] ||  Jellyfin_Conf=$JELLYFIN_CONF
-        clear
-
-        MediaserverConf="$Jellyfin_Conf"
-        break
-        ;;
-      "Plex")
-        MediaserverService="plexmediaserver"
-        MediaserverUser="plwx"
-
-        echo "Enter the path to the Jellyfin file directory."
-        echo "Usually: ${Plex_Conf}"
-        echo ""
-        read -p "Enter a directory or press ENTER if the file directory is ${Plex_Conf}: " PLEX_CONF
-
-        [ -z "$PLEX_CONF" ] ||  Plex_Conf=$PLEX_CONF
-        clear
-
-        MediaserverConf="$Plex_Conf"
-        break
-        ;;
-      *)
-        echo "Invalid option, try again."
-        ;;
-    esac
-  done
-
-  clear 
-
-  mkdir -p $HOME/Scripts
-  cd $HOME/Scripts
-
-  tee -a ./"${BackupRestoreConf}" <<EOF
-# Configuration for Backup-Restore scripts
-
-# TODO: The uuid of the backup drive
-uuid='$uuid'
-
-# TODO: The Backup Drive Mount Point
-BackupDir='$BackupDir'
-
-# TODO: The service name of the web server. Used to start/stop web server (e.g. 'systemctl start <webserverServiceName>')
-webserverServiceName='$webserverServiceName'
-
-# TODO: The directory of your Nextcloud installation (this is a directory under your web root)
-NextcloudConfig='$NextcloudConfig'
-
-# TODO: The directory of your Nextcloud data directory (outside the Nextcloud file directory)
-# If your data directory is located in the Nextcloud files directory (somewhere in the web root),
-# the data directory must not be a separate part of the backup
-NextcloudDataDir='$NextcloudDataDir'
-
-# TODO: The name of the database system (one of: mysql, mariadb, postgresql)
-# 'mysql' and 'mariadb' are equivalent, so when using 'mariadb', you could also set this variable to 'mysql' and vice versa.
-DatabaseSystem='$DatabaseSystem'
-
-# TODO: Your Nextcloud database name
-NextcloudDatabase='$NextcloudDatabase'
-
-# TODO: Your Nextcloud database user
-DBUser='$DBUser'
-
-# TODO: The password of the Nextcloud database user
-DBPassword='$DBPassword'
-
-# TODO: The service name of the media server. Used to start/stop web server (e.g. 'systemctl start <mediaserverServiceName>')
-MediaserverService='$MediaserverService'
-
-# TODO: The service name of the media server. Used to restore permissions media server settings)
-MediaserverUser='$MediaserverUser'
-
-# TODO: The directory where the Media Server settings are stored (this directory is stored within /var/lib)
-MediaserverConf='$MediaserverConf'
-
-# Log File
-LogFile="$LogFile"
-
-EOF
-
-  clear
-
-  wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Nextcloud%20%2B%20Media%20server%20/Backup.sh
-  wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Nextcloud%20%2B%20Media%20server%20/Restore.sh
-  
-  chmod 700 *.sh
 
   clear
 
@@ -495,21 +311,38 @@ while true; do
     1)
       disk_backup
       backup
+      wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/Backup.sh
+      wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/Restore.sh
+      chmod 700 *.sh
+      clear
       cron
       ;;
     2)
       disk_backup
       nextcloud
+      wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Nextcloud/Backup.sh
+      wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Nextcloud/Restore.sh
+      chmod 700 *.sh
+      clear
       cron
       ;;
     3)
       disk_backup
-      configure_mediaserver
+      mediaserver
+      wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Media%20Server/Backup.sh
+      wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Media%20Server/Restore.sh
+      chmod 700 *.sh
+      clear
       cron
       ;;      
     4)
       disk_backup
-      configure_nextcloud_mediaserver
+      nextcloud
+      mediaserver
+      wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Nextcloud%20%2B%20Media%20server%20/Backup.sh
+      wget https://raw.githubusercontent.com/edsonsbj/Backup-Restore/main/scripts/Nextcloud%20%2B%20Media%20server%20/Restore.sh
+      chmod 700 *.sh
+      clear
       cron
       ;;
     5)
